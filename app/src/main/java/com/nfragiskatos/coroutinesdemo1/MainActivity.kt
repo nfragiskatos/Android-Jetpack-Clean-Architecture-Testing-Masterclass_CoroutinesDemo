10f1d3e3a9f7bb9ac2a49717f301fe3087e5e53c
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-19 20:14:32 -0700

    Implementing Parallel Decomposition to have both long running processes run in parallel: - Create a Coroutine scope with a launch builder, and using Dispatchers.Main, because we are creating a Toast message, so we need control of the UI. - Call each long running suspend function by using the ASYNC builder with the Dispatchers.IO. Each ASYNC returns to us a Deferred<Int> - Use the Deferred.away() function to wait for both suspend functions to finish, and then continue execution the returned values are available.

--- app/src/main/java/com/nfragiskatos/coroutinesdemo1/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/coroutinesdemo1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.nfragiskatos.coroutinesdemo1
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
@@ -12,14 +14,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnCount.setOnClickListener {
-            tvCount.text = count++.toString()
+        CoroutineScope(Dispatchers.Main).launch {
+            Log.i("MyTag", "Calculation Starting...")
+
+            val stock1: Deferred<Int> = async(Dispatchers.IO) { getStock1() }
+            val stock2: Deferred<Int> = async(Dispatchers.IO) { getStock2() }
+            val total = stock1.await() + stock2.await()
+            Toast.makeText(applicationContext, "Total Value: $total", Toast.LENGTH_LONG).show()
+            Log.i("MyTag", "Total is $total")
         }
-        btnDownloadUserData.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
-                downloadUserData()
-            }
-        }
+
+//        btnCount.setOnClickListener {
+//            tvCount.text = count++.toString()
+//        }
+//        btnDownloadUserData.setOnClickListener {
+//            CoroutineScope(Dispatchers.IO).launch {
+//                downloadUserData()
+//            }
+//        }
     }
 
     private suspend fun downloadUserData() {
@@ -33,4 +45,16 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+}
+
+private suspend fun getStock1() : Int {
+    delay(10000)
+    Log.i("MyTag", "stock 1 returned")
+    return 55000
+}
+
+private suspend fun getStock2() : Int {
+    delay(8000)
+    Log.i("MyTag", "stock 2 returned")
+    return 35000
 }
